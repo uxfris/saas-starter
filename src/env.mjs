bcc72e3101f8ba9e302626f9c66f699f01e8ccee
Fris <fris.info@icloud.com>
2025-11-03 05:17:08 -0500

    chore: set up folder structure

---
+++ src/env.mjs
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    OPENAI_API_KEY: z.string(),
+    STRIPE_SECRET_KEY: z.string(),
+    SUPABASE_SERVICE_ROLE_KEY: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  },
+  runtimeEnv: process.env,
+});
